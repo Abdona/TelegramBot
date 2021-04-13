963eb7a410b7d4190df0bc92574a6662984fdc28
Abdulrahman <boodynaser@rocketmail.com>
2021-04-13 16:48:06 +0200

    Initialize Bot

---
+++ bin/main.rb
@@ -0,0 +1,14 @@
+require 'telegram/bot'
+
+token = '1778305517:AAGu-u69L4AW8IpA4uDJzZEuul1Qjc5xsOk'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
