51921ae2a06a90687f2df5c36fd5612aeaf3e004
Abdulrahman <boodynaser@rocketmail.com>
2021-04-15 00:42:37 +0200

    Add test for translating from english to german

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,10 +1,23 @@
 require './lib/assistant'
-
+require './lib/google_translate'
 describe Assistant do
+  let(:test) { Assistant.new }
   describe '#information' do
     it 'check information' do
-      test = Assistant.new
       expect(test.information).to include(/\w/)
+    end
+  end
+  describe '#translate' do
+    it 'check translate' do
+      expect(test.translate('hello')).to eql('Hallo')
+    end
+  end
+end
+describe Translate do
+  let(:test) { Translate.new }
+  describe '#to_german' do
+    it 'check to_geramn function' do
+      expect(test.to_german('hello')).to eql('Hallo')
     end
   end
 end
