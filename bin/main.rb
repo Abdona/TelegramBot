6318d718676589bf76fcdbe586cb7d83cb84db17
Abdulrahman <boodynaser@rocketmail.com>
2021-04-15 16:06:09 +0200

    Fix rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -9,7 +9,6 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      chatid = message.chat.id 
       bot.api.send_message(chat_id: message.chat.id, text: "Hallo, #{message.from.first_name}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Tschüss, #{message.from.first_name}")
